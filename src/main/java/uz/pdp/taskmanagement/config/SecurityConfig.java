bc9480a4ded4c6a7f9c3a83e5f78dedf973dd42f
soh1b003 <sohibjinnorboboyev@gmail.com>
2024-09-09 11:54:40 +0500

    block and unblock full

--- src/main/java/uz/pdp/taskmanagement/config/SecurityConfig.java
+++ src/main/java/uz/pdp/taskmanagement/config/SecurityConfig.java
@@ -12,7 +12,7 @@ import uz.pdp.taskmanagement.filter.JwtFilter;
 
 @Configuration
 public class SecurityConfig {
-    private final String[] WHITE_LIST = {"/register", "/login", "/"};
+    private final String[] WHITE_LIST = {"/company", "/company/**", "/register", "/login", "/"};
 
     @Autowired
     private JwtFilter jwtFilter;
@@ -27,7 +27,7 @@ public class SecurityConfig {
                             .requestMatchers("/user").hasRole("CEO")
 
                             .requestMatchers("/features").hasRole("PRODUCT_OWNER")
-                            .requestMatchers("/company").hasRole("PRODUCT_OWNER")
+//                            .requestMatchers("/company/**").hasRole("PROJECT_ADMINISTRATOR")
                             .requestMatchers("/user/CEO").hasRole("PROJECT_ADMINISTRATOR")
                             .requestMatchers("/product/**").hasRole("CEO")
                             .requestMatchers("/team/**").hasRole("CEO")
